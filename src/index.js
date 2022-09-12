0a4908e995aa13cd011f01768873cebc0835f52c
Jepunen <62857023+Jepunen@users.noreply.github.com>
2022-09-12 18:39:56 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import "./styles.css";
+
+const table = document.getElementById("table");
+const submitBtn = document.getElementById("submit-data");
+const clearTableBtn = document.getElementById("empty-table");
+const imageElement = document.getElementById("input-image");
+
+let usernamesInTable = ["Webmaster", "User123", "AnotherUser222"];
+
+submitBtn.addEventListener("click", () => addRowToTable());
+clearTableBtn.addEventListener("click", () => {
+  table.innerHTML =
+    '<table id="table" style="text-align: left;"><tr><th>Username</th><th>Email</th><th>Address</th><th>Admin</th><th>Image</th></tr></table>';
+  usernamesInTable = [];
+});
+
+// Help with insertRow: https://www.w3schools.com/jsref/met_table_insertrow.asp
+function addRowToTable() {
+  console.log("Added a row to table");
+
+  let username = document.getElementById("input-username").value;
+  let email = document.getElementById("input-email").value;
+  let address = document.getElementById("input-address").value;
+  let admin = document.getElementById("input-admin").value;
+
+  if (usernamesInTable.includes(username)) {
+    let rowOfUsername = usernamesInTable.indexOf(username);
+    let usernameRow = document.getElementById("table-row-" + rowOfUsername);
+
+    console.log(usernameRow.cells);
+
+    usernameRow.cells[0].innerHTML = username;
+    usernameRow.cells[1].innerHTML = email;
+    usernameRow.cells[2].innerHTML = address;
+    usernameRow.cells[3].innerHTML = admin;
+
+    let image = document.createElement("img");
+    image.src = URL.createObjectURL(imageElement.files[0]);
+    image.height = 64;
+    image.width = 64;
+    usernameRow.cells[4].appendChild(image);
+  } else {
+    var newRow = table.insertRow(-1);
+    var usernameCell = newRow.insertCell();
+    var emailCell = newRow.insertCell();
+    var AddressCell = newRow.insertCell();
+    var AdminCell = newRow.insertCell();
+    var imageCell = newRow.insertCell();
+
+    newRow.setAttribute("id", "table-row-" + table.rows.length - 2);
+    usernameCell.innerHTML = username;
+    emailCell.innerHTML = email;
+    AddressCell.innerHTML = address;
+    AdminCell.innerHTML = admin;
+
+    let image = document.createElement("img");
+    image.src = URL.createObjectURL(imageElement.files[0]);
+    image.height = 64;
+    image.width = 64;
+    imageCell.appendChild(image);
+
+    usernamesInTable.push(document.getElementById("input-username").value);
+  }
+  console.log(usernamesInTable);
+}
